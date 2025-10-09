5d485e3c38c044b90fe2aaa6fc7bc212016e44b2
Dainis Dilevka <thedenisovan@proton.me>
2025-10-09 14:18:39 +0100

    Add: custom theme btn

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,5 +1,6 @@
 import { useContext } from 'react';
 import { ThemeContext } from '../App';
+import svg from '../../utils/svg';
 
 export default function Header() {
   const { dark, toggleDark } = useContext(ThemeContext);
@@ -13,9 +14,21 @@ export default function Header() {
       <h1>header</h1>
       <button
         onClick={() => toggleDark()}
-        className='dark:bg-white dark:text-black border-1'
+        className={`bg-white border-1 w-[4rem] h-[2rem] rounded-2xl flex align-middle`}
       >
-        {dark ? 'Light' : 'Dark'}
+        {dark ? (
+          <img
+            width='30px'
+            className='rotate-180 translate-x-[31px] duration-200'
+            src={svg.sun}
+          />
+        ) : (
+          <img
+            className='-translate-x-[0px] duration-200'
+            width='30px'
+            src={svg.moon}
+          />
+        )}
       </button>
     </header>
   );
---
+++ src/utils/svg.ts
@@ -0,0 +1,9 @@
+import sun from '../assets/sun.svg';
+import moon from '../assets/moon.svg';
+
+const svg = {
+  sun,
+  moon,
+};
+
+export default svg;
