236e8c99c51f973997a7b97c7cf6c677c4511045
Dainis Dilevka <thedenisovan@proton.me>
2025-10-12 14:57:10 +0100

    Add: state for input fields

--- src/components/main/auth/Authorization.tsx
+++ src/components/main/auth/Authorization.tsx
@@ -7,10 +7,37 @@ type InputProps = {
   id: string;
   label: string;
   type?: string;
+  value: string;
+  updateInput: (e: string, id: string) => void;
+};
+
+type FormData = {
+  mail: string;
+  pass: string;
+  passConfirm: string;
 };
 
 export default function Authorization() {
   const [isSignin, setSignIn] = useState<boolean>(true);
+
+  const [formData, setFormData] = useState<FormData>({
+    mail: '',
+    pass: '',
+    passConfirm: '',
+  });
+
+  const eraseInput = () =>
+    setFormData({
+      mail: '',
+      pass: '',
+      passConfirm: '',
+    });
+
+  const updateInput = (e: string, id: string) =>
+    setFormData({
+      ...formData,
+      [id]: e,
+    });
 
   // return appropriate text based on current page
   const returnText = (option1: string, option2: string) =>
@@ -27,13 +54,24 @@ export default function Authorization() {
       <p className='text-center !font-normal italic'>
         {returnText('Lets continue our journey', 'Sign up to get started')}
       </p>
-      <SignInForm isSignIn={isSignin} returnText={returnText} />
+      <SignInForm
+        formData={formData}
+        updateInput={updateInput}
+        isSignIn={isSignin}
+        returnText={returnText}
+      />
 
       <div className='flex flex-col'>
         <p className='text-center mt-3 mb-0'>
           {returnText("Don't have an account?", 'Have an account all ready?')}
         </p>
-        <button onClick={() => changeAuthPage()} className='underline'>
+        <button
+          onClick={() => {
+            changeAuthPage();
+            eraseInput();
+          }}
+          className='underline'
+        >
           {returnText('Sign up', 'Sign in')}
         </button>
       </div>
@@ -41,12 +79,17 @@ export default function Authorization() {
   );
 }
 
+// component for sign up form
 function SignInForm({
   isSignIn,
   returnText,
+  formData,
+  updateInput,
 }: {
   isSignIn: boolean;
   returnText: (val1: string, val2: string) => string;
+  formData: FormData;
+  updateInput: (e: string, id: string) => void;
 }) {
   return (
     <form
@@ -55,11 +98,27 @@ function SignInForm({
       }}
       className='flex flex-col !m-[2rem] gap-2'
     >
-      <FormInput id='mail' type='email' label='Email' />
-      <FormInput id='pass' label='Password' />
+      <FormInput
+        updateInput={updateInput}
+        id='mail'
+        value={formData.mail}
+        type='email'
+        label='Email'
+      />
+      <FormInput
+        updateInput={updateInput}
+        id='pass'
+        value={formData.pass}
+        label='Password'
+      />
       {/* if user is on sign up page add extra input el for password confirmation */}
       {!isSignIn && (
-        <FormInput id='pass-confirm' label='Password confirmation' />
+        <FormInput
+          updateInput={updateInput}
+          id='passConfirm'
+          value={formData.passConfirm}
+          label='Password confirmation'
+        />
       )}
 
       <p className='text-center mb-0'>Or</p>
@@ -77,11 +136,27 @@ function SignInForm({
   );
 }
 
-function FormInput({ id, label, type = 'password' }: InputProps) {
+// component for single input and label element
+function FormInput({
+  id,
+  label,
+  type = 'password',
+  value,
+  updateInput,
+}: InputProps) {
   return (
     <div className='flex flex-col'>
       <label htmlFor={id}>{label}</label>
-      <input className='rounded-[8px]' type={type} id={id} required />
+      <input
+        onChange={(e) => {
+          updateInput(e.target.value, e.target.id);
+        }}
+        value={value}
+        className='rounded-[8px] text-black'
+        type={type}
+        id={id}
+        required
+      />
     </div>
   );
 }
