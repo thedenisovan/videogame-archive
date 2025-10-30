5cff79bab4bd67f532e4c4826d43b3ffa4e1244e
Dainis Dilevka <thedenisovan@proton.me>
2025-10-30 08:57:29 +0000

    Fix: footer collapsing on large screens

--- src/components/footer/FooterComp.tsx
+++ src/components/footer/FooterComp.tsx
@@ -10,32 +10,34 @@ export default function FooterComp() {
       container
       className={`p-4 ${
         !dark ? '!bg-gray-200' : '!bg-gray-800 text-white'
-      } rounded-0 max-w-[1600px] lg:max-w-[2300px] m-auto shadow-none`}
+      } rounded-0 shadow-none w-[100%]`}
     >
-      <FooterCopyright
-        className={`${
-          dark ? 'text-white' : 'text-black'
-        } text-[1.1rem] lg:!text-[1.5rem]`}
-        by=' VAULT33™'
-        year={2025}
-      />
-      <FooterLinkGroup className='flex justify-center align-middle gap-5 pt-2 mb-0'>
-        <FooterLink
-          children='GitHub'
-          href='https://github.com/thedenisovan?tab=overview&from=2025-08-01&to=2025-08-12'
-          dark={dark}
+      <div className='m-auto max-w-[1600px] lg:max-w-[2300px] md:flex md:items-center md:!justify-between md:gap-45'>
+        <FooterCopyright
+          className={`${
+            dark ? 'text-white' : 'text-black'
+          } text-[1.1rem] lg:!text-[1.5rem]`}
+          by=' VAULT33™'
+          year={2025}
         />
-        <FooterLink
-          children='LinkedIn'
-          href='https://www.linkedin.com/in/dainis-dilevka-961a332b4/'
-          dark={dark}
-        />
-        <FooterLink
-          children='Licensing'
-          href='https://rawg.io/apidocs?utm_source=chatgpt.com#terms/'
-          dark={dark}
-        />
-      </FooterLinkGroup>
+        <FooterLinkGroup className='flex justify-center align-middle gap-5 pt-2 md:!pt-0 mb-0 '>
+          <FooterLink
+            children='GitHub'
+            href='https://github.com/thedenisovan?tab=overview&from=2025-08-01&to=2025-08-12'
+            dark={dark}
+          />
+          <FooterLink
+            children='LinkedIn'
+            href='https://www.linkedin.com/in/dainis-dilevka-961a332b4/'
+            dark={dark}
+          />
+          <FooterLink
+            children='Licensing'
+            href='https://rawg.io/apidocs?utm_source=chatgpt.com#terms/'
+            dark={dark}
+          />
+        </FooterLinkGroup>
+      </div>
     </Footer>
   );
 }
